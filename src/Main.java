8fe4d2d30c498b128147fdcd42e4eb4b30e0f533
DaniyalMShehzad <daniyalshehzad305@gmail.com>
2023-01-13 11:56:56 +0500

    java

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+// first program
+public class Main {
+    public static void main(String[] args) {
+        // output
+//        System.out.println("Hello Java!");
+//        System.out.println("hello world! ");
+
+        //variable
+//        String name = "saqib";
+//        int age = 17;
+//        String friend ="sattar";
+//        String BestFriend = friend;
+//        System.out.println(BestFriend );
+
+        //JAVA type of two
+
+        //first type primitive
+
+//       byte - 1byte ki memory leta hai [-128 to 127]
+            byte age = 17 ;
+        System.out.println(age);
+
+//        short - 2bytes memory leta hai
+
+//        int - 4bytes memory leta hai (1,2,3,4,5,6 integars)
+           int phone = 1234567890;
+        System.out.println(phone);
+
+//        long - 8bytes memory leta hai
+            long phone2 = 12345678900L;
+        System.out.println(phone2);
+
+//        float - 4bytes memory leta hai (3.14 point wali value pi hoti hai)
+                 float pi = 4153.33F;
+        System.out.println(pi);
+
+//        double -8bytes memory leta hai
+
+//         char -2bytes memory leta hai(a,b,c,d,e,f,@,# ,)
+            char letter = '@';
+        System.out.println(letter);
+
+//          boolean = 1 bytes memory leta hai (true/false)
+
+        //second type non primitive /refrence
+
+    }
+}
